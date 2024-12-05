e7456d02a8416fa1826be67d460e0baecd7a3034
Diurno <Diurno@DAM116>
2024-12-05 12:35:40 +0100

    sf

---
+++ programacion/src/metodos/ejercicios/Ejercicios02_01.java
@@ -0,0 +1,15 @@
+package metodos.ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicios02_01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+	}
+
+}
