fc1738d84603f6aefd9a37e8af9549194acef019
Diurno <Diurno@DAM116>
2024-12-19 10:51:28 +0100

    32

--- programacion/src/metodos/ejercicios/Ejercicios04_01.java
+++ programacion/src/metodos/ejercicios/Ejercicios04_01.java
@@ -7,19 +7,25 @@ public class Ejercicios04_01 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int n, relleno;
+		int n;
 		
 		System.out.println("Cuantas entradas desea introducir:");
 		n = Integer.parseInt(sc.nextLine());
+		
+		mostrar(n);
+		
+		sc.close();
+	}
+	
+	public static void mostrar(int n) {
+		
+		int relleno;
 		
 		for(int i = 1; i <= n; i++) {
 			relleno = (int) (Math.random() * 2000) + 1;
 			System.out.println(relleno + " " + codigo(relleno));
 		}
-		
-		sc.close();
 	}
-	
 	
 	public static int[] separar(int relleno) {
 		int acc = 0, n = relleno;
@@ -47,6 +53,9 @@ public class Ejercicios04_01 {
 			
 			if(i % 2 == 0) {
 				cod += (digitos[i] * 3 + min_dcha(digitos, i));
+				
+			} else {
+				cod += (digitos[i] * 2 + max_izq(digitos, i));
 			}
 		}
 		
@@ -56,7 +65,7 @@ public class Ejercicios04_01 {
 	public static int min_dcha(int[] digitos, int index) {
 		int menor;
 		
-		if(index == digitos.length -1) {
+		if(index == digitos.length - 1) {
 			menor = 0;
 			
 		} else {
@@ -70,6 +79,25 @@ public class Ejercicios04_01 {
 			}
 		}	
 		return menor;
+	}
+	
+	public static int max_izq(int[] digitos, int index) {
+		int mayor;
+		
+		if(index == 0) {
+			mayor = 0;
+			
+		} else {
+			mayor = digitos[index - 1];
+			
+			for(int i = index - 1; i >= 0; i--) {
+				
+				if(digitos[i] > mayor) {
+					mayor = digitos[i];
+				}
+			}
+		}	
+		return mayor;
 	}
 }
 
---
+++ programacion/src/metodos/ejercicios/Ejercicios05_01.java
@@ -0,0 +1,10 @@
+package metodos.ejercicios;
+
+public class Ejercicios05_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
